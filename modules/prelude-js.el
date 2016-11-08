52af97cd521e6f579ecc239b8b41561a0d958414
Matthew Billie <matthewbillienyc@gmail.com>
2016-11-08 14:41:43 -0500

    some custom changes

--- modules/prelude-js.el
+++ modules/prelude-js.el
@@ -36,7 +36,7 @@
 (prelude-require-packages '(js2-mode json-mode))
 
 (require 'js2-mode)
-
+(add-to-list 'auto-mode-alist '("\\.js.jsx'"  . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'"    . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.pac\\'"   . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
--- personal/custom.el
+++ personal/custom.el
@@ -15,13 +15,14 @@
  '(hl-paren-colors
    (quote
     ("#B9F" "#B8D" "#B7B" "#B69" "#B57" "#B45" "#B33" "#B11")))
+ '(js-indent-level 2)
  '(linum-format "%3i")
  '(nrepl-message-colors
    (quote
     ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (flycheck-swift spacegray-theme swift-mode geiser json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company smex ido-ubiquitous flx-ido vkill exec-path-from-shell zop-to-char zenburn-theme volatile-highlights undo-tree smartrep smartparens operate-on-number move-text magit projectile ov guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl easy-kill diminish diff-hl discover-my-major browse-kill-ring anzu ace-window ace-jump-buffer ace-jump-mode)))
+    (auto-complete-mode geiser json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company smex ido-ubiquitous flx-ido vkill exec-path-from-shell zop-to-char zenburn-theme volatile-highlights undo-tree smartrep smartparens operate-on-number move-text magit projectile ov guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl easy-kill diminish diff-hl discover-my-major browse-kill-ring anzu ace-window ace-jump-buffer ace-jump-mode)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(powerline-color1 "#3d3d68")
  '(powerline-color2 "#292945")
@@ -46,7 +47,10 @@
      (320 . "#8CD0D3")
      (340 . "#94BFF3")
      (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
+ '(vc-annotate-very-old-color "#DC8CC3")
+ '(web-mode-attr-indent-offset 2)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-css-indent-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -67,18 +71,21 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/personal/sass-mode.el"))
 (autoload 'scss-mode "scss-mode")
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(add-to-list 'auto-mode-alist '("\\.js.jsx\\'" . js-jsx-mode))
 (require 'web-mode)
 (setq web-mode-enable-auto-pairing nil)
 (global-linum-mode)
 (global-unset-key "\C-\\")
-(setq global-whitespace-mode 0)
+;; (setq global-whitespace-mode 0)
 (setq ring-bell-function 'ignore)
 (setq prelude-whitespace nil)
-(disable-theme 'deeper-blue)
-(load-theme 'soft-charcoal t)
-(load-theme 'colonoscopy t)
+;; (load-theme 'soft-charcoal t)
+;;(load-theme 'colonoscopy t)
+(autoload 'auto-complete-mode "auto-complete-mode")
 (windmove-default-keybindings)
+(global-auto-complete-mode 1)
 (require 'helm-config)
 (helm-mode 1)
 (require 'prelude-helm-everywhere)
+(load-theme 'deeper-blue)
 0
